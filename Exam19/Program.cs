4e8a640721b44998a2c5f30389e36fcd74fe55b6
Nikolay <labazopelastra@gmail.com>
2022-12-08 13:29:50 +0700

    Add new file

---
+++ Exam19/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите 5-тизначное число");  //12321
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 10000)
+{
+    System.Console.WriteLine(number + " Не является 5-тизначным числом");
+}
+else
+{
+    if (number / 10000 == number % 10)
+    {
+        if (number / 1000 % 10 == number % 100 / 10)
+        {
+            System.Console.WriteLine("Это палиндром!");
+        }
+    }
+    else
+    {
+        System.Console.WriteLine("Это не палиндром.");
+    }
+}
